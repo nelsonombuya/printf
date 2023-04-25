05fd6891c0c422137ba13dcf96a60e8438a4bbb6
nelsonombuya <nelson.ombuya@zohomail.com>
2023-04-25 12:55:09 -0700

    _printf.c

---
+++ print_hex.c
@@ -0,0 +1,126 @@
+#include "main.h"
+
+/**
+ * print_hex - Prints an unsigned int in hexidecimal format
+ * @n: Unsigned int to print
+ * @c: Flag to determine case of printing (0 = lower, 1 = upper)
+ * Return: Number of digits that are printed
+ */
+int print_hex(unsigned int n, unsigned int c)
+{
+	unsigned int a[8];
+	unsigned int i, m, sum;
+	char diff;
+	int count;
+
+	m = 268435456; /* (16 ^ 7) */
+	if (c)
+		diff = 'A' - ':';
+	else
+		diff = 'a' - ':';
+	a[0] = n / m;
+	for (i = 1; i < 8; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0, sum = 0, count = 0; i < 8; i++)
+	{
+		sum += a[i];
+		if (sum || i == 7)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + diff + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * print_x - Takes an unsigned int and prints it in lowercase hex format
+ * @x: Unsigned int to print
+ * Return: Number of digits printed
+ */
+int print_x(va_list x)
+{
+	return (print_hex(va_arg(x, unsigned int), 0));
+}
+
+/**
+ * print_X - Takes am unsigned int and prints it in uppercase hex format
+ * @X: Unsigned int to print
+ * Return: Number of digits printed
+ */
+int print_X(va_list X)
+{
+	return (print_hex(va_arg(X, unsigned int), 1));
+}
+
+/**
+ * _pow - Calculates the exponent of the parameter
+ * @base: Base of the Exponent
+ * @exponent: Exponent of the number
+ * Return: @base ^ @exponent
+ */
+static unsigned long _pow(unsigned int base, unsigned int exponent)
+{
+	unsigned int i;
+	unsigned long ans = base;
+
+	for (i = 1; i < exponent; i++)
+	{
+		ans *= base;
+	}
+	return (ans);
+}
+
+/**
+ * print_p - Prints an Address
+ * @p: Address to print
+ * Return: Number of characters to print
+ */
+int print_p(va_list p)
+{
+	int count = 0;
+	unsigned int a[16];
+	unsigned int i, sum;
+	unsigned long n, m;
+	char *str = "(nil)";
+
+	n = va_arg(p, unsigned long);
+	if (n == 0)
+	{
+		for (i = 0; str[i]; i++)
+		{
+			_putchar(str[i]);
+			count++;
+		}
+		return (count);
+	}
+	_putchar('0');
+	_putchar('x');
+	count = 2;
+	m = _pow(16, 15); /* 16 ^ 15 */
+	a[0] = n / m;
+	for (i = 1; i < 16; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0, sum = 0; i < 16; i++)
+	{
+		sum += a[i];
+		if (sum || i == 15)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + ('a' - ':') + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
